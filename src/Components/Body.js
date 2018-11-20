8425c961b1b247053e4661f74ebd6c0ed4c30b22
Chris Kirkham <chris.kirkham@endurance.com>
2018-11-19 17:38:08 -0700

    adding some components

---
+++ src/Components/Body.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Body extends Component {
+render() {
+    return (
+        <article className="App-body">
+        <p>
+            This is where the text will go
+        </p>
+        </article>
+        )
+    }   
+};
+
+export default Body;
---
+++ src/Components/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+render() {
+    return (
+        <footer className="App-footer">
+        <p>
+            Footer text and whatnot
+        </p>
+        </footer>
+        )
+    }   
+};
+
+export default Footer;

f7c3d4cd4dcceb3fd8b50e2f1a41f66a137d81c8
Chris Kirkham <me@chriskirkham.com>
2019-01-16 22:24:53 -0700

    Adding basic scaffold

---
+++ src/Components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+    <header className="App-header">
+          <p>
+              Squires Family Cookbook
+           </p>
+        </header>
+    );
+}
+
+export default Header;

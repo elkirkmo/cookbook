d0aefdc058dce64c8377975ff4f2a754bcd9998f
Chris Kirkham <me@chriskirkham.com>
2020-12-19 10:56:52 -0700

    chore(): added tests

--- src/utils/captalize.ts
+++ src/utils/captalize.ts
@@ -1,8 +1,17 @@
-export const capitalize = (string: String) => {
-  return string
-    .split('')
-    .map((y, i) => (i === 0 ? y.toUpperCase() : y))
-    .join('');
+export const capitalize = (s: string) => {
+  /***
+   * Returns a formatted sentence where the first character of every word is capitalizaed and all other letters are lowercase
+   * params(s: string)
+   */
+  return s
+    .split(' ')
+    .map((x) =>
+      x
+        .split('')
+        .map((y, i) => (i === 0 ? y.toUpperCase() : y.toLowerCase()))
+        .join('')
+    )
+    .join(' ');
 };
 
 export default capitalize;
